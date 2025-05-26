1169b0d8541453cf08f28bfe04fbd835da2d00c0
trannhon70 <kevintran351996@gmail.com>
2025-05-26 08:26:27 +0700

    update

--- admin/login.php
+++ admin/login.php
@@ -15,6 +15,9 @@ $class = new users();
 <html>
 <head>
     <title>Login</title>
+      <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Phòng khám chuyên khoa">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
